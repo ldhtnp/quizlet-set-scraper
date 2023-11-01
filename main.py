5a07b1d812963fbb17528c36dec16683fe65916f
Luke Hendrickson <64616402+ldhtnp@users.noreply.github.com>
2023-10-31 20:17:46 -0500

    added comments, type hinting, and format_content

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from typing import TypedDict
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet, Tag
 
 
 class Cards(TypedDict):
@@ -10,66 +10,110 @@ class Cards(TypedDict):
     answer: str
 
 
-def export_set(cards_in_set):
-    export_file_name = "quizlet.txt"
-    separation_character = "\t"
+cards_in_set: list[Cards] = []
+url_file_name: str = "url.txt"
+export_file_name: str = "quizlet.txt"
+separation_character: str = "\t"
+
+
+def export_set() -> None:
+    """
+    Purpose:    Export the contents of 'cards_in_set' to a text file.
+    Modifies:   Nothing
+    Returns:    None
+    """
     try:
-        with open(export_file_name, 'w', encoding='utf-8') as file: 
+        with open(export_file_name, "w", encoding="utf-8") as file:
             for card in cards_in_set:
-                file.write(card['question'].strip() + separation_character + card['answer'].strip() + "\n")
+                file.write(
+                    card["question"].strip()
+                    + separation_character
+                    + card["answer"].strip()
+                    + "\n"
+                )
         print(f"\nSuccessfully exported to: {export_file_name}")
     except Exception as e:
         print("Error occurred when exporting cards:", e)
 
 
-def scrape_cards(cards_in_set, driver):
-    html_content = load_html(driver)
+def format_content(elements: ResultSet[Tag]) -> None:
+    """
+    Purpose:    Properly format the card content removing special characters and whitespace
+    Modifies:   Elements in 'cards_in_set'
+    Returns:    None
+    """
+    print('Formatting content...')
+    try:
+        for element in elements:
+            element.string = element.text.strip()
+            #print(element.string)
+    except Exception as e:
+        print("Exception occurred when formatting content:", e)
+
+
+def scrape_cards(driver) -> None:
+    """
+    Purpose:    Scrape quizlet set, properly format content and populate 'cards_in_set'.
+    Modifies:   Populates 'cards_in_set'
+    Returns:    None
+    """
+    html_content: str = load_html(driver=driver)
     print("Scraping the quizlet set...")
     try:
-        soup = BeautifulSoup(html_content, 'html.parser')
-        
+        soup: BeautifulSoup = BeautifulSoup(html_content, "html.parser")
+
         # Find all <span> elements with class "TermText"
-        elements = soup.find_all('span', class_='TermText')
-        
+        elements: ResultSet[Tag] = soup.find_all("span", class_="TermText")
+        format_content(elements=elements)
+
         for i in range(0, len(elements), 2):
-            question = elements[i].text.replace('<br>', ' ')
-            answer = elements[i + 1].text.replace('<br>', ' ')
-            card = {'question': question, 'answer': answer}
+            question: str = elements[i].string
+            answer: str = elements[i + 1].string
+            card: dict[str, str] = {"question": question, "answer": answer}
             cards_in_set.append(card)
     except Exception as e:
         print("Error occurred when scraping cards:", e)
 
 
-def load_html(driver):
+def load_html(driver) -> str:
+    """
+    Purpose:    Loads the HTML content from the provided web page
+    Modifies:   Nothing
+    Returns:    source_html (string object)
+    """
     print("Loading the html...")
     try:
-        source_html = driver.page_source
+        source_html: str = driver.page_source
         return source_html
     except Exception as e:
         print("Error occurred when loading html:", e)
 
 
-def main():
-    url_file_name = "url.txt"
+def main() -> None:
+    """
+    Purpose:    w
+    Modifies:   Nothing
+    Returns:    None
+    """
     try:
-        with open(url_file_name, 'r') as file:
+        with open(url_file_name, "r") as file:
             url = file.readline()
         print("Quizlet link:", url)
         try:
             opts = Options()
             opts.headless = True
-            geckodriver_path = 'geckodriver.exe' # Assumes geckdriver is in the same directory
+            geckodriver_path = (
+                "geckodriver.exe"  # Assumes geckdriver is in the same directory
+            )
             service = Service(geckodriver_path)
 
             driver = webdriver.Firefox(service=service, options=opts)
             driver.get(url)
-            # Might need to insert time.sleep() here
-            
-            cards_in_set: Cards = []
+
             try:
-                scrape_cards(cards_in_set, driver)
+                scrape_cards(driver=driver)
                 driver.quit()
-                export_set(cards_in_set)
+                export_set()
             except:
                 driver.quit()
         except Exception as e:
@@ -78,5 +122,6 @@ def main():
     except Exception as e:
         print("Error occurred when loading url:", url)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
